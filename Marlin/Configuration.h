464e1aa712abb18d4289864bf5d71c71ea74667f
Cory Forseth <forsethc@gmail.com>
2023-02-12 16:13:57 -0600

    lower default z-offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1316,7 +1316,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -31.8, -40.5, -.5 }  // Ender3S1 Configs
+#define NOZZLE_TO_PROBE_OFFSET { -31.8, -40.5, -.4 }  // Ender3S1 Configs
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
